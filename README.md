d6c6fd33e541a65818bbbe258183b4e8bf9de034
Conor Hastings <hastings.conorm@gmail.com>
2016-02-10 00:12:23 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## get-emoji
+
+give emoji name, get emoji image
+
+### Install
+
+`npm install get-emoji --save`
+
+### Use
+
+```js
+import getEmoji from 'get-emoji';
+const rage = getEmoji(rage);
+```
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+	entry: './lib/index.js',
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'index.js',
+		library: 'get-emoji',
+		libraryTarget: 'umd'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js?$/,
+				loader: require.resolve('babel-loader'),
+				include: path.join(__dirname, 'lib'),
+				query: {
+					presets: ['es2015', 'stage-2']
+				}
+			},
+			{ test: /\.(png|jpg)$/, loader: 'url-loader' }
+		]
+	},
+	plugins: [
+		new webpack.optimize.UglifyJsPlugin({
+		  compressor: {
+		    pure_getters: true,
+		    unsafe: true,
+		    unsafe_comps: true,
+		    screw_ie8: true,
+		    warnings: false
+		   }
+		})
+	]
+};
